173679f76caf327c1bed75366d8b48585ea40c43
zacasatru <ignacio.bernardez@estudiante.uam.es>
2024-03-26 20:14:17 +0100

    load file completed

--- data_load.py
+++ data_load.py
@@ -2,8 +2,6 @@ import csv
 from typing import (
     Dict,
     List,
-    Set,
-    Tuple
 )
 import random
 
@@ -12,9 +10,12 @@ filename = 'ChatGPT-evaluation-v2.txt'
 
 
 def take_n_random_elements(data: List, n: int) -> List:
+    """Take n random elements from a list of data"""
     return random.sample(data, n)
 
 class Evaluation:
+    """Class to store the evaluation of an argument"""
+
     claim: int
     claim_premise: int = None
     aspect: int = None
@@ -25,6 +26,7 @@ class Evaluation:
     emotional_ethic: int = None
 
     def __init__(self, claim: str, claim_premise: str, aspect: str, premise_validation: str, coherence: str, consistence: str, persuasion: str, emotional_ethic: str) :
+        """Initialize the evaluation of an argument"""
         self.claim = int(claim)
         if claim == '1':
             self.claim_premise = int(claim_premise)
@@ -37,45 +39,65 @@ class Evaluation:
                 self.emotional_ethic = int(emotional_ethic)
 
     def get_field(self, field_name: str) -> int:
+        """Get the value of a field of the evaluation"""
         return getattr(self, field_name)
     
-    def format(self):
+    def format_print(self):
+        """Format the evaluation to print it in a human-readable way"""
         cad = "Claim: " + str(self.claim) + ', Claim+premise: ' + str(self.claim_premise) + ', Aspect related: ' + str(self.aspect) + ', Premise validation: ' + str(self.premise_validation) + ', Coherence: ' + str(self.coherence) + ', Consistence: ' + str(self.consistence) + ', Persuasion: ' + str(self.persuasion) + ', Emotional/ethic: ' + str(self.emotional_ethic)
+        return cad
+    
+    def format_file(self, fields: List[str]):
+        """Format the evaluation to save it in a file with the specified fields"""
+        cad = '\t'.join([str(self.get_field(field)) for field in fields])
         return cad
         
     
 class EvaluatedArgument:
+    """Class to store an argument with its evaluation"""
+
     def __init__(self, id_proposal: int, id_argument: int, argument: str, evaluation: Evaluation):
+        """Initialize the argument with its evaluation"""
         self.id_proposal = id_proposal
         self.id_argument = id_argument
         self.argument = argument
         self.evaluation = evaluation
 
-    def __str__(self):
-        return self.evaluation
     def get_evaluation(self):
+        """Get the evaluation of the argument"""
         return self.evaluation
-    def format(self):
-        cad = "\tArgument id: " + str(self.id_argument) + '\n\tArgument: ' + self.argument + '\n\tEvaluation: ' + self.evaluation.format() + '\n\n'
+    def format_print(self):
+        """Format the argument to print it in a human-readable way"""
+        cad = "\tArgument id: " + str(self.id_argument) + '\n\tArgument: ' + self.argument + '\n\tEvaluation: ' + self.evaluation.format_print() + '\n\n'
+        return cad
+    def format_file(self, fields: List[str]):
+        """Format the argument to save it in a file with the specified fields"""
+        cad = str(self.id_argument) + '\t' + self.argument + '\t'+ self.evaluation.format_file(fields)
         return cad
     
 class Corpus:
+    """Class to store a corpus of arguments with their evaluations"""
 
     arguments_dict: Dict[int, List[EvaluatedArgument]]
 
     def __init__(self):
+        """Initialize the corpus of arguments"""
         self.arguments_dict = {}
+
     def _append_argument(self, proposal_id: int,argument: EvaluatedArgument):
+        """Append an argument to the corpus of arguments"""
         if proposal_id in self.arguments_dict:
             self.arguments_dict[proposal_id].append(argument)
         else:
             self.arguments_dict[argument.id_proposal] = [argument]
+
     def append_all_arguments(self, proposal_id: int, arguments: List[EvaluatedArgument]):
+        """Append a list of arguments to the corpus of arguments"""
         for argument in arguments:
             self._append_argument(proposal_id, argument)
-    def get_evaluated_arguments(self, proposal_id: int):
-        return self.arguments_dict[proposal_id]
+    
     def filter_by_completed(self, field_name: str, value: int):
+        """Filter the corpus of arguments by the value of a field of the evaluation of the arguments, completed with the same number of non-arguments"""
         filtered_corpus = Corpus()
         for proposal_id in self.arguments_dict.keys():
             arguments = self.arguments_dict[proposal_id]
@@ -88,18 +110,34 @@ class Corpus:
             filtered_corpus.append_all_arguments(proposal_id, non_arguments)
         return filtered_corpus
     
-    def format(self):
+    def format_print(self):
+        """Format the corpus of arguments to print it in a human-readable way"""
         cad = ""
         for proposal_id in self.arguments_dict.keys():
             cad += ("Proposal id: " + str(proposal_id) + '\n\n')
             arguments = self.arguments_dict[proposal_id]
             for argument in arguments:
-                cad += argument.format()
+                cad += argument.format_print()
 
         return cad
 
+    def format_file(self, fields: List[str]):
+        """Format the corpus of arguments to save it in a file with the specified fields"""
+        cad = "proposal_id\targument_id\targument\t" + '\t'.join(fields) + "\n"
+        for proposal_id in self.arguments_dict.keys():
+            arguments = self.arguments_dict[proposal_id]
+            for argument in arguments:
+                cad += str(proposal_id) + '\t' + argument.format_file(fields) + '\n'
+        return cad
+    
+    def save_to_file(self, filename: str, fields: List[str] = ['claim', 'claim_premise', 'aspect', 'premise_validation', 'coherence', 'consistence', 'persuasion', 'emotional_ethic']):
+        """Save the corpus of arguments in a file with the specified fields"""
+        with open(filename, 'w', encoding = 'utf-8') as file:
+            file.write(self.format_file(fields))
+
     
     def filter_by(self, field_name: str, value: int):
+        """Filter the corpus of arguments by the value of a field of the evaluation of the arguments"""
         filtered_corpus = Corpus()
         for proposal_id in self.arguments_dict.keys():
             arguments = self.arguments_dict[proposal_id]
@@ -109,6 +147,7 @@ class Corpus:
 
 #optimized, only one iteration
 def classify_arguments_or_not_opt():
+    """Classify the arguments as arguments or not arguments based on the value of the field 'Claim+premise?WHY?' of the evaluation of the arguments"""
     proposal_id = -1
     corpus = Corpus()
     with open(filename, 'r', newline='') as file:
@@ -139,6 +178,7 @@ def classify_arguments_or_not_opt():
     
 #optimized, only one iteration
 def classify_arguments_or_not_with_aspect_opt():
+    """Classify the arguments as arguments or not arguments based on the value of the fields 'Claim+premise?WHY?' and 'Relacionado con aspecto?' of the evaluation of the arguments"""
     proposal_id = -1
     corpus = Corpus()
     with open(filename, 'r', newline='') as file:
@@ -169,17 +209,20 @@ def classify_arguments_or_not_with_aspect_opt():
 
 #not optimized, two iterations
 def classify_arguments_or_not():
+    """Classify the arguments as arguments or not arguments based on the value of the field 'Claim+premise?WHY?' of the evaluation of the arguments"""
     corpus = create_complete_corpus()
     filtered_corpus = corpus.filter_by('claim_premise', 1)
     return filtered_corpus
 
 #not optimized, two iterations
 def classify_arguments_or_not_with_aspect():
+    """Classify the arguments as arguments or not arguments based on the value of the fields 'Claim+premise?WHY?' and 'Relacionado con aspecto?' of the evaluation of the arguments"""
     corpus = create_complete_corpus()
     filtered_corpus = corpus.filter_by('claim_premise', 1).filter_by('aspect', 1)
     return filtered_corpus
 
 def create_complete_corpus():
+    """Create a corpus with all the arguments and non-arguments"""
     proposal_id = -1
     corpus = Corpus()
     with open(filename, 'r', newline='') as file:
@@ -198,9 +241,6 @@ def create_complete_corpus():
         return corpus
 
 
-
-
-print(classify_arguments_or_not().filter_by('persuasion', 2).format())
 
         
     
